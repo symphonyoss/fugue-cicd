76f245b346eb54989759adf0fc320de09dfeaf4a
bruceskingle <bruce.skingle@symphony.com>
2018-09-21 02:22:14 -0700

    W

--- src/org/symphonyoss/s2/fugue/cicd/v1/CreateEnvironmentTypeTask.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v1/CreateEnvironmentTypeTask.groovy
@@ -129,5 +129,12 @@ CreateEnvironmentTypeTask Finished
     }
     
     steps_.echo 'Cluster ' + cluster_ + ' does not exist, creating...'
+    
+    def createdCluster = steps_.readJSON(text:
+      steps_.sh(returnStdout: true, script: 'aws ecs create-clusters --region us-east-1 --cluster-name ' + cluster_ ))
+
+    clusterArn_ = createdCluster."cluster"."clusterArn"
+    
+    steps_.echo 'created clusterArn_ ' + clusterArn_
   }
 }
