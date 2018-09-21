b9e3e2a3358ccb76ea3f697d6b131f01504b9d4f
bruceskingle <bruce.skingle@symphony.com>
2018-09-21 05:19:05 -0700

    W

--- src/org/symphonyoss/s2/fugue/cicd/v1/EnvironmentTypeConfig.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v1/EnvironmentTypeConfig.groovy
@@ -9,9 +9,14 @@ class EnvironmentTypeConfig implements Serializable
   {
     steps_ = steps;
     
+    steps.echo 'TT1'
+    steps.echo 'environmentType=' + environmentType
     steps_.sh 'pwd'
+    steps.echo 'TT2'
     steps_.sh 'ls'
+    steps.echo 'TT3'
     steps_.sh 'ls config/environment/' + environmentType
+    steps.echo 'TT4'
     def config = steps_.readJSON file:'config/environment/' + environmentType + '/environmentType.json'
     
     steps_.echo 'config=' + config
