376334f01f8edb22bbd1e9419656b2417281f2a5
bruceskingle <bruce.skingle@symphony.com>
2018-09-24 00:21:28 -0700

    W

--- src/org/symphonyoss/s2/fugue/cicd/v1/JenkinsTask.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v1/JenkinsTask.groovy
@@ -7,13 +7,14 @@
 package org.symphonyoss.s2.fugue.cicd.v1
 
 import org.jenkinsci.plugins.workflow.cps.DSL
+import org.jenkinsci.plugins.workflow.cps.EnvActionImpl
 
 class JenkinsTask
 {
-  private def     env_
-  private DSL     steps_
+  private EnvActionImpl     env_
+  private DSL               steps_
   
-  public JenkinsTask(env, steps)
+  public JenkinsTask(EnvActionImpl env, DSL steps)
   {
     env_            = env
     steps_          = steps
@@ -29,11 +30,11 @@ class JenkinsTask
   public void execute()
   {
     steps_.echo 'JenkinsTask.execute()'
-//    steps_.echo 'env_ = ' + env_.getClass()
+    steps_.echo 'env_ = ' + env_.getClass()
     steps_.echo 'steps_ = ' + steps_.getClass()
     
     
-//    echo '2 env_ = ' + env_.getClass()
+    echo '2 env_ = ' + env_.getClass()
     echo '2 steps_ = ' + steps_.getClass()
   }
 }
