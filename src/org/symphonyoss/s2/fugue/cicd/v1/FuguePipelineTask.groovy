d8c0a3df057dc209648320bd65dc1c52f62d07d6
bruceskingle <bruce.skingle@symphony.com>
2018-09-24 00:24:18 -0700

    W

--- src/org/symphonyoss/s2/fugue/cicd/v1/FuguePipelineTask.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v1/FuguePipelineTask.groovy
@@ -8,23 +8,23 @@ package org.symphonyoss.s2.fugue.cicd.v1
 
 class FuguePipelineTask extends JenkinsTask
 {
-  private def     env_
-  private def     steps_
+//  private def     env_
+//  private def     steps_
   private FuguePipeline pipeLine_
   
   public FuguePipelineTask(env, steps, FuguePipeline pipeLine)
   {
     super(env, steps)
-      steps_          = steps
-    pipeLine_       = pipeLine
+//      steps_          = steps
+//    pipeLine_       = pipeLine
   }
   
   public void execute()
   {
     super.execute()
     
-    steps_.echo 'FuguePipelineTask.execute()'
-    steps_.echo 'pipeLine_ = ' + pipeLine_.getClass()
+    echo 'FuguePipelineTask.execute()'
+    echo 'pipeLine_ = ' + pipeLine_.getClass()
     
   }
 }
