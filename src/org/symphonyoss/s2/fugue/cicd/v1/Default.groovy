383fb5f76b5df7496e735f2fe32c02d6e58b13a9
bruceskingle <bruce.skingle@symphony.com>
2018-09-30 11:12:21 -0700

    W

---
+++ src/org/symphonyoss/s2/fugue/cicd/v1/Default.groovy
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2018 Symphony Communication Services, LLC.
+ *
+ * All Rights Reserved
+ */
+
+package org.symphonyoss.s2.fugue.cicd.v1
+
+class Default
+{
+  public String  value(String var, String defaultValue)
+  {
+    if(var==null || "".equals(var.trim()))
+      return defaultValue
+    else
+      return var
+  }
+}
