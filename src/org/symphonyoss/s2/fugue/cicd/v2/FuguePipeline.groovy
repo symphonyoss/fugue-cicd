4365d25259295d373b9f53ed7b8fcdb3076c3986
Bruce Skingle <bruce.skingle@symphony.com>
2019-01-06 17:36:08 -0800

    Add current base images (#11)

--- src/org/symphonyoss/s2/fugue/cicd/v2/FuguePipeline.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v2/FuguePipeline.groovy
@@ -604,6 +604,9 @@ environmentType ${environmentType}
         {
                         
           sh "docker pull 189141687483.dkr.ecr.${awsRegion}.amazonaws.com/symphony-es/base-java8:latest"
+          sh "docker pull 189141687483.dkr.ecr.${awsRegion}.amazonaws.com/symbase/java8:latest"
+          sh "docker pull 189141687483.dkr.ecr.${awsRegion}.amazonaws.com/symbase/java10:latest"
+          sh "docker pull 189141687483.dkr.ecr.${awsRegion}.amazonaws.com/symbase/java11:latest"
           
           
           steps.withCredentials([steps.file(credentialsId: 'maven-settings', variable: 'FILE')]) {
