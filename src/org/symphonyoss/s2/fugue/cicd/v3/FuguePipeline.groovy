b450fbf953f4ba30c076d31b56cd469a4687e593
bruceskingle <bruce.skingle@symphony.com>
2019-07-30 17:00:33 -0700

    W

--- src/org/symphonyoss/s2/fugue/cicd/v3/FuguePipeline.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v3/FuguePipeline.groovy
@@ -598,7 +598,7 @@ serviceGitBranch is ${serviceGitBranch}
   
   public void createBuildIdClass()
   {
-    File f = new File(steps.envVars.get('WORKSPACE') + "/src/main/java/fugue/BuildId.java");
+    File f = new File(environ.get('WORKSPACE') + "/src/main/java/fugue/BuildId.java");
     
     try
     {
