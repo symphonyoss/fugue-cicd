ae27d6f8eb286790339ec3ad2e5ef1b52eec232a
GeremiaLongobardo <geremia.longobardo@symphony.com>
2020-10-14 10:38:37 +0200

    ddddd

--- src/org/symphonyoss/s2/fugue/cicd/v4/FuguePipeline.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v4/FuguePipeline.groovy
@@ -1019,7 +1019,6 @@ environmentType ${environmentType}
       {
        
         echo ('download starting ' + fullUrl)
-        sh 'pwd'
         
         File ff = new File('test')
         ff.mkdir()
