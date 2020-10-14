a22651226cd2921966016a3591f28c37e58e2244
GeremiaLongobardo <geremia.longobardo@symphony.com>
2020-10-14 08:42:06 +0200

    test

--- src/org/symphonyoss/s2/fugue/cicd/v4/FuguePipeline.groovy
+++ src/org/symphonyoss/s2/fugue/cicd/v4/FuguePipeline.groovy
@@ -1022,6 +1022,7 @@ environmentType ${environmentType}
         File ff = new File('test')
         ff.mkdir()
         if(ff.exists())
+          echo 'Created dir'
         File f = new File(filename)
         
         if(f.exists())
